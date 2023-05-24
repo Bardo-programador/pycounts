816f0a8bbcb5b676c99a9177c990b5bce4a918c2
Samuel Roberto <samucadeus23@gmail.com>
2023-05-24 13:43:15 -0300

    Funcoes de contagem

---
+++ src/pycounts/pycounts.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from string import punctuation
+
+def loud_file(input_file):
+    with open(input_file, "r") as file:
+        return file.read()
+        
+def clean_text(texto):
+    texto = texto.lower()
+    for p in punctuation:
+        texto = texto.replace(p, "")
+    return texto
+    
+def count_words(input_file):
+    texto = loud_file(input_file)
+    texto = clean_text(texto)
+    palavras = texto.split()
+    return Counter(palavras)
